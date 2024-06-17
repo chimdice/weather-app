80725c1c48400cf9277a4d1e977aa873584b21ed
chimdice <118031439+chimdice@users.noreply.github.com>
2024-06-17 12:31:31 -0700

    Add ui

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const weatherApi = 'http://api.weatherapi.com/v1/current.json?key=94855a49c8a14bf3a8841418241706&q=';
+const form = document.querySelector('form');
+const tempShower = document.querySelector('.temp-shower');
+const locationhower = document.querySelector('.location-shower');
 
 const getLocactionWeather = async function (location) {
     try {
@@ -6,17 +9,18 @@ const getLocactionWeather = async function (location) {
         const response = await fetch(fullApi, {mode:'cors'});
         const locationData = await response.json();
 
-        const tempF = locationData.current.temp_f;
-        const tempC = locationData.current.temp_c;
+        const tempF = locationData.current.temp_f+'F';
+        const tempC = locationData.current.temp_c+'C';
         const fullLocation = `${locationData.location.name}, ${locationData.location.country}`;
 
+        tempShower.textContent = tempF;
+        locationhower.textContent = fullLocation
         console.log(tempF, tempC, fullLocation);
     } catch {
         (error) => {console.log(error)};
     };
 };
 
-const form = document.querySelector('form');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
